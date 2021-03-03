3d2fd0ffdf9690945e342257e05afbd6cf1e1a9f
prammathal <prammathal02@gmail.com>
2021-03-04 01:03:47 +0530

    [Prammathal]Inheritance concepts

---
+++ InhertianceConcepts/Laptop.java
@@ -0,0 +1,36 @@
+package javaLearningsAndAssignments.InhertianceConcepts;
+
+public class Laptop {
+
+    public final String Name;
+    public final int Price;
+    public final String Processor;
+    public final int Ram;
+    public final int HardDrive;
+
+    public Laptop(String Name, int Price, String Processor, int Ram, int HardDrive) {
+        this.Name = Name;
+        this.Price = Price;
+        this.Processor = Processor;
+        this.Ram = Ram;
+        this.HardDrive = HardDrive;
+    }
+
+    public void printLaptopDetails() {
+        String hddString;
+        if(HardDrive < 1000)
+        {
+            hddString = " HDD: "+HardDrive+"GB";
+        }
+        else
+        {
+            hddString = " HDD: "+HardDrive/1000+"TB";
+        }
+        System.out.println("Name: "+Name+ " Price: $"+Price+ " Processor: "+Processor+" Ram: "+Ram+"GB"+ hddString);
+
+    }
+
+
+}
+
+
---
+++ InhertianceConcepts/LaptopChild.java
@@ -0,0 +1,20 @@
+package javaLearningsAndAssignments.InhertianceConcepts;
+
+ class LapTopChild extends Laptop {
+    public LapTopChild(String Name, int Price, String Processor, int Ram, int HardDrive) {
+        super(Name, Price, Processor,Ram, HardDrive);
+    }
+
+     public static void main(String[] args) {
+         //Print the laptop details
+         LapTopChild lenovo=new LapTopChild("Lenovo",1000,"i3",2,500);
+         lenovo.printLaptopDetails();
+
+         LapTopChild dell=new LapTopChild("Dell",1500,"i5",4,1000);
+         dell.printLaptopDetails();
+
+         LapTopChild sony =new LapTopChild("Sony",2000,"i9",8,1000);
+         sony.printLaptopDetails();
+
+     }
+}
